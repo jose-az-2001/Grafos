c76c44e43fbdce4fe61325ad788d8db6a475744a
Jose Daniel Az <josedanielxela@gmail.com>
2022-06-08 11:31:36 -0600

    Create Grafos.py
    Inicio

---
+++ Grafos.py
@@ -0,0 +1,38 @@
+#programa para clase que implemente grafos
+
+import numpy as np
+
+#metodos
+#con este método se establece un arco o adyacencia entre nodos indicando el 
+#vértice de origen, vértice de destino y el peso o costo de dicho arco
+def arco(nodo_i, nodo_f, costo):
+    None
+    
+#se invoca a la matriz de adyacencia del grafo
+def adyacencia():
+    None
+
+#con este método se invoca a la lista de adyacencia del grafo
+def listaAdyacencia():
+    None
+
+#con este método se invoca a la matriz de costos del grafo
+def costos():
+    None
+
+#con este método se indica el vértice a partir del cual se debe iniciar la búsqueda del 
+#circuito de Hamilton y desplegar la lista que indique la trayectoria a seguir y el costo de dicha trayectoria.
+def hamilton(inicio):
+    None
+    
+#con este método se aplica el algoritmo de Dijkstra indicando el nodo de inicio y 
+#nodo de finalización, debe desplegar la matriz / vector de costos en cada iteración 
+# del algoritmo y al finalizar mostrar el vector/matriz de la trayectoria a seguir y costo final.
+def dijkstra(nodo_i, nodo_f):
+    None
+    
+
+
+#Constructor
+def Grafo(vertices):
+    None
